fa62400d1957a1fdd55493a624c89cda80605f49
Campbell Suter <znix@znix.xyz>
2018-01-18 16:33:06 +1300

    ifdef windows-specific colouring code in logging system

--- src/util/util.h
+++ src/util/util.h
@@ -136,6 +136,8 @@ bool ExtractZIPArchive(const std::string& path, const std::string& extractPath);
 #define PD2HOOK_TRACE_FUNC_MSG(msg)
 #endif
 
+#ifdef _WIN32
+
 #define PD2HOOK_LOG_LEVEL(msg, level, file, line, ...) do { \
 	unsigned int color = 0; \
 	for (auto i : {__VA_ARGS__}) { \
@@ -152,6 +154,19 @@ bool ExtractZIPArchive(const std::string& path, const std::string& extractPath);
 		writer << msg; \
 		writer.write(logger); \
 	}} while (false);
+
+#elif __GNUC__
+
+// TODO UNIX text colouring
+#define PD2HOOK_LOG_LEVEL(msg, level, file, line, ...) do { \
+	auto& logger = pd2hook::Logging::Logger::Instance(); \
+	if(level >= logger.getLoggingLevel()) { \
+		pd2hook::Logging::LogWriter writer(file, line, level); \
+		writer << msg; \
+		writer.write(logger); \
+	}} while (false);
+
+#endif
 
 #define PD2HOOK_LOG_FUNC(msg) PD2HOOK_LOG_LEVEL(msg, pd2hook::Logging::LogType::LOGGING_FUNC, __FILE__, 0, FOREGROUND_BLUE, FOREGROUND_GREEN, FOREGROUND_INTENSITY)
 #define PD2HOOK_LOG_LOG(msg) PD2HOOK_LOG_LEVEL(msg, pd2hook::Logging::LogType::LOGGING_LOG, __FILE__, __LINE__, FOREGROUND_BLUE, FOREGROUND_GREEN, FOREGROUND_INTENSITY)
