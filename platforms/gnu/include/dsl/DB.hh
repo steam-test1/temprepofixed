d080ec7e28488608c41d3dbf49af459d9c240568
Campbell Suter <znix@znix.xyz>
2019-03-20 20:51:23 +1300

    Change the dsl::DB layout to match that of some recent update

--- platforms/gnu/include/dsl/DB.hh
+++ platforms/gnu/include/dsl/DB.hh
@@ -6,21 +6,12 @@
 
 namespace dsl
 {
-	class DB
-	{
-	public:
-		void *ptr1;
-		void *allocator_thingy;
-		void *ptr2;
-		void *another_allocator_thingy;
-		blt::libcxxstring some_str_1;
-		blt::libcxxstring some_str_2;
-		void *ptr4;
-		void *some_map;
-		dsl::FileSystemStack *stack;
-		void *ptr5;
-		void *ptr6;
-	};
+   class DB
+   {
+   public:
+      char null1[184];
+      dsl::FileSystemStack *stack;
+   };
 };
 
 /* vim: set ts=3 softtabstop=0 sw=3 expandtab: */
